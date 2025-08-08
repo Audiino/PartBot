e415ee0cb6e8c3b3aca23e6c0a61ba9f4ddf0b87
Audiino <121476415+Audiino@users.noreply.github.com>
2025-08-09 01:39:12 +0700

    games: Separate and functionify EV render

--- src/ps/games/explodingvoltorb/render.tsx
+++ src/ps/games/explodingvoltorb/render.tsx
@@ -5,8 +5,9 @@ import { CardType, CardDescription } from '@/ps/games/explodingvoltorb/constants
 
 import type { ExplodingVoltorb } from '@/ps/games/explodingvoltorb';
 import type { Log } from '@/ps/games/explodingvoltorb/logs';
-import type { RenderCtx } from '@/ps/games/explodingvoltorb/types';
+import type { GamePhase, RenderCtx } from '@/ps/games/explodingvoltorb/types';
 import type { ReactElement, ReactNode } from 'react';
+import React from 'react';
 
 type This = { msg: string };
 
@@ -77,7 +78,7 @@ function UserPanel({ children }: { children: ReactNode }): ReactElement {
 	);
 }
 
-function PlayerHands({ players }: { players: RenderCtx['players'] }): ReactElement[] {
+function PlayerHandAmount({ players }: { players: RenderCtx['players'] }): ReactElement[] {
 	return Object.values(players).map(player => {
 		const username = <Username name={player.name} />;
 		return (
@@ -88,88 +89,116 @@ function PlayerHands({ players }: { players: RenderCtx['players'] }): ReactEleme
 	});
 }
 
-export function render(this: This, ctx: RenderCtx): ReactElement {
-	return (
-		<center>
-		
-		<UserPanel>
-			<div>Draw pile: {pluralize(ctx.board.drawPileAmount, 'card', 'cards')}</div>
-			<div>Discard pile: {pluralize(ctx.board.discardPileAmount, 'card', 'cards')}</div>
-			<div>
-				Last played:{" "}
-				{Object.entries(ctx.board.discardPileLastPlayedAmount)
-					.map(([card, count]) => `${card}: ${count}`)
-					.join(", ")}
-			</div>
-			<hr />
-			<PlayerHands players={ctx.players} />
-		</UserPanel>
+function PlayerHand(
+	{ isActive, hand, selection, msg }: 
+	{ isActive: boolean; hand: CardType[] | undefined; selection: { cards: boolean[] }; msg: string; }):
+	ReactElement | null {
+		if (!hand) return null;
 
-		{ctx.side ? (
-			
-			
-			// {ctx.hasSelectedCards ? (
-			// You have selected: some logic ish here to get the cards
-			// ): null}			
-			// The effect is/This selection is invalid
-			// confirm (or doesn't exist if invalid)
-			<>
-			{ctx.selection.hasAny ? (
-				<UserPanel>
-					<div>You have selected {pluralize(ctx.selection.cardAmount, 'card', 'cards')}: {ctx.selection.cardNames.join(", ")}</div>
-				</UserPanel>
-			) : null}
-
+		return (
 			<UserPanel>
 				<div>
-					{ctx.hand?.map((card, i) => (
+					{hand?.map((card, i) => (
 						<div key={i} style={{ margin: '2px 0' }}>
 							<details>
 								<summary style={{ cursor: 'pointer' }}>
-									{ctx.selection.cards[i] ? (
+									{isActive ? (
 										<>
-											<Button value={`${this.msg} ! s ${i}`}><b>{card}</b></Button>
-										</>										
-									) : (
-										<>										
-										<Button value={`${this.msg} ! s ${i}`}>{card}</Button>
+										{selection.cards[i] ? (
+												<Button value={`${msg} ! s ${i}`}><b>{card}</b></Button>
+										) : (
+												<Button value={`${msg} ! s ${i}`}>{card}</Button>
+										)}
 										</>
-									)}									
+									) : (
+										<>{card}</>
+									)}
 								</summary>
 								<div style={{ border: '1px solid', borderRadius: 4, padding: '4px 8px', marginTop: 4 }}>
-									{CardDescription[card]}<br></br>									
+									{CardDescription[card]}<br></br>
 								</div>
 							</details>
 						</div>
 					))}
 				</div>
 			</UserPanel>
+		)
+}
 
-			{ctx.isActive ? (
-				<>       
-				{ctx.phase === 'Voltorb reaction' ? (
-					<UserPanel>  
-						<div>You drew a Voltorb!</div>
-						{ctx.hand?.includes(CardType.DEFUSE) ? (
-							<Form value={`${this.msg} ! r {replaceVoltorb}`} style={{ margin: '4px 0' }}>
-								<input name="replaceVoltorb" placeholder="1 for top most position" width="100" style={{ marginRight: 4 }} />
-								<button>Replace</button>
-							</Form>
-						) : (
-							<div>Rip you have no Defuse cards, you exploded.</div>
-						)}
-					</UserPanel>
-				) : null }
-					<UserPanel>                              
-						<Button value={`${this.msg} ! d`} style={{ border: '2px solid darkred', borderRadius: 4 }}>
-							End turn and draw
-						</Button>                                                      
-					</UserPanel>
+function CardSelection({ isActive, selection }: { isActive: boolean; selection: { cardNames: CardType[]; cardAmount: number; hasAny: boolean; } }): ReactElement | null {
+	if (!isActive) return null;
+	if (selection.hasAny) return (
+		<UserPanel>
+			<div>You have selected {pluralize(selection.cardAmount, 'card', 'cards')}: {selection.cardNames.join(", ")}</div>
+		</UserPanel>
+	)
+	else return (
+		null
+	)	
+}
+
+function EndTurnAndDraw({ isActive, phase, msg }: { isActive: boolean; phase: GamePhase; msg: string }): ReactElement | null {
+	if (!isActive) return null;
+	if (phase === 'Voltorb reaction') return null;
+
+	return (
+		<UserPanel>
+			<Button value={`${msg} ! d`} style={{ border: '2px solid darkred', borderRadius: 4 }}>
+				End turn and draw
+			</Button>
+		</UserPanel>
+	)
+}
+
+function VoltorbReaction(
+	{ isActive, phase, hand, msg }: 
+	{ isActive: boolean; phase: GamePhase; hand: CardType[] | undefined; msg: string }
+	): ReactElement | null {
+	if (!isActive) return null;
+	if (phase !== 'Voltorb reaction') return null;
+	
+	return (		
+		<UserPanel>  
+			<div>You drew a Voltorb!</div>
+			{hand?.includes(CardType.DEFUSE) ? (
+				<Form value={`${msg} ! r {replaceVoltorb}`} style={{ margin: '4px 0' }}>
+					<input name="replaceVoltorb" placeholder="1 for top most position" width="100" style={{ marginRight: 4 }} />
+					<button>Replace</button>
+				</Form>
+			) : (
+				<div>Rip you have no Defuse cards, you exploded.</div>
+			)}
+		</UserPanel>		
+	)
+}
+
+export function render(this: This, ctx: RenderCtx): ReactElement {
+	return (
+		<center>
+			<UserPanel>
+				<div>Draw pile: {pluralize(ctx.board.drawPileAmount, 'card', 'cards')}</div>
+				<div>Discard pile: {pluralize(ctx.board.discardPileAmount, 'card', 'cards')}</div>
+				<div>
+					Last played:{" "}
+					{Object.entries(ctx.board.discardPileLastPlayedAmount)
+						.map(([card, count]) => `${card}: ${count}`)
+						.join(", ")}
+				</div>
+				<hr />
+				<PlayerHandAmount players={ctx.players}/>
+			</UserPanel>
+			
+			{ctx.side ? (
+				// TODO: The effect of CardSelection is: (CardDescription)
+				// Invalid play
+				// confirm (or doesn't exist if invalid)
+				<>
+				<VoltorbReaction isActive={ctx.isActive} phase={ctx.phase} hand={ctx.hand} msg={this.msg}/>
+				<CardSelection isActive={ctx.isActive} selection={ctx.selection}/>		
+				<PlayerHand isActive={ctx.isActive} hand={ctx.hand} selection={ctx.selection} msg={this.msg}/>
+				<EndTurnAndDraw isActive={ctx.isActive} phase={ctx.phase} msg={this.msg}/>			
 				</>
-			) : null}
-			</>
-		) : null}        
-		
+			) : null}		
 		</center>
 	);
 }
